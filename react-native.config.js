25dcdc7ce0630d63301f42fcfa28b3eea97ba5f6
Chirag Rami <chirag.rami@mindinventory.com>
2024-05-17 11:44:27 +0530

    update config

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dependencies: [
+    'react-native-gesture-handler',
+    'react-native-reanimated',
+    'react-native-screens',
+    'react-native-safe-area-context',
+    'lottie-ios',
+    'lottie-react-native',
+    'luxon',
+    'react-native-linear-gradient',
+    '@react-native-community/slider',
+    '@react-native-community/datetimepicker',
+    '@passiolife/nutritionai-react-native-sdk-v3',
+  ],
+};
